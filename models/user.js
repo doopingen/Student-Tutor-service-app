727efa7ba7395951c4d8e4105f7b4d1a352c0cee
Garrison28 <ghighsmith28@gmail.com>
2019-11-08 09:46:33 -0800

    update

--- models/user.js
+++ models/user.js
@@ -12,19 +12,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'You must enter a password'],
     minlength: [8, 'Password must be between 8 and 128 characters'],
-    maxlength: [128, 'Password must be between 8 and 128 characters']
+    maxlength: [128, 'Password must be between 8 and 128 characters'],
   },
   email: {
     type: String,
     required: [true, 'You must enter an email'],
     minlength: [5, 'Email must be between 5 and 99 characters'],
     maxlength: [99, 'Email must be between 5 and 99 characters']
-  },
-  username: String,
-  role: String,
-  bio: String,
-  subjects: [subjectSchema],
-  messages: [messageSchema]
+  }
 });
 
 userSchema.set('toObject', {
@@ -37,7 +32,6 @@ userSchema.set('toObject', {
     return returnJson;
   }
 });
-
 userSchema.methods.authenticated = function(password) {
   return bcrypt.compareSync(password, this.password);
 };
